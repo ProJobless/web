fbd07e046451bc8fce900b2faad1986e89f66fd8
John Sparwasser <johnspar1@gmail.com>
2013-01-14 14:16:50 -0500

    Fixed the copyright positioning

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,11 +1,15 @@
-<div class="copyright-container">
-	<p>© 2013 John Sparwasser</p>
-</div>
+	</div>
 
-</body>
+	<div id="footer">
+		<p>© 2013 John Sparwasser</p>
+	</div>
+
+</div>
 
 <script type="text/javascript" src="<?php echo base_url();?>scripts/jquery.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>scripts/mashtagg_globals.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>scripts/mashtagg.js"></script>
+
+</body>
 
 </html>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -10,61 +10,66 @@
 </head>
 
 <body>
-<?php if ($u = Current_User::user()):?>
-<div id="top-bar">
-	<div id = "container1">
-		<div id = "mashtagg"><p><a href = "<?php echo base_url();?>">mashtagg</a></p></div>
-		<div id = "container3">
-			<div id="container4">
-				<div class="link-box feed-link">
-					<p><a href = "<?php echo base_url();?>feed">FEED</a></p>
+
+<div id="body">
+
+	<?php if ($u = Current_User::user()):?>
+	<div id="header">
+		<div id = "container1">
+			<div id = "mashtagg"><p><a href = "<?php echo base_url();?>">mashtagg</a></p></div>
+			<div id = "container3">
+				<div id="container4">
+					<div class="link-box feed-link">
+						<p><a href = "<?php echo base_url();?>feed">FEED</a></p>
+					</div>
+					<div class="link-box blog-link">
+						<p><a href = "<?php echo base_url();?>blog">BLOG</a></p>
+					</div>
+					<div class="link-box profile-link">
+						<p><a href = "<?php echo base_url() . 'profile'; ?>">PROFILE</a></p>
+					</div>
+					<div class="link-box tags-link">
+						<p><a href = "<?php echo base_url();?>tags">TAGS</a></p>
+					</div>
+					<div class="link-box users-link">
+						<p><a href = "<?php echo base_url();?>users">USERS</a></p>
+					</div>
+					<div class="link-box saved-link">
+						<p><a href = "<?php echo base_url();?>saved">SAVED</a></p>
+					</div>
+					<div class="link-box settings-link">
+						<p><a href = "<?php echo base_url();?>settings">SETTINGS</a></p>
+					</div>
+					<div class="link-box about-link">
+						<p><a href = "<?php echo base_url();?>about">ABOUT</a></p>
+					</div>
+					<div style="clear:both"></div>
 				</div>
-				<div class="link-box blog-link">
-					<p><a href = "<?php echo base_url();?>blog">BLOG</a></p>
-				</div>
-				<div class="link-box profile-link">
-					<p><a href = "<?php echo base_url() . 'profile'; ?>">PROFILE</a></p>
-				</div>
-				<div class="link-box tags-link">
-					<p><a href = "<?php echo base_url();?>tags">TAGS</a></p>
-				</div>
-				<div class="link-box users-link">
-					<p><a href = "<?php echo base_url();?>users">USERS</a></p>
-				</div>
-				<div class="link-box saved-link">
-					<p><a href = "<?php echo base_url();?>saved">SAVED</a></p>
-				</div>
-				<div class="link-box settings-link">
-					<p><a href = "<?php echo base_url();?>settings">SETTINGS</a></p>
-				</div>
-				<div class="link-box about-link">
-					<p><a href = "<?php echo base_url();?>about">ABOUT</a></p>
-				</div>
-				<div style="clear:both"></div>
+
 			</div>
-
+		</div>
+		<div id = "container2">
+			<div id = "user_control_box"><p>Hello <em><?php echo $u['username']; ?></em></p></div>
+			<div id = "logout"><p><?php echo anchor('logout','Logout'); ?></p></div>
 		</div>
 	</div>
-	<div id = "container2">
-		<div id = "user_control_box"><p>Hello <em><?php echo $u['username']; ?></em></p></div>
-		<div id = "logout"><p><?php echo anchor('logout','Logout'); ?></p></div>
-	</div>
-</div>
 
 
-<?php else: ?>
+	<?php else: ?>
 
-<div id="top-bar">
-	<div id = "container1">
-		<div id = "slasht"><p><a href = "<?php echo base_url();?>">Slasht</a></p></div>
-		<div id = "container3">
-			<p>
-				<a href = "<?php echo base_url();?>tags">TAGS</a> |
-				<a href = "<?php echo base_url();?>users">USERS</a> |
-				<a href = "<?php echo base_url();?>about">ABOUT</a>
-			</p>
+	<div id="header">
+		<div id = "container1">
+			<div id = "slasht"><p><a href = "<?php echo base_url();?>">Slasht</a></p></div>
+			<div id = "container3">
+				<p>
+					<a href = "<?php echo base_url();?>tags">TAGS</a> |
+					<a href = "<?php echo base_url();?>users">USERS</a> |
+					<a href = "<?php echo base_url();?>about">ABOUT</a>
+				</p>
+			</div>
 		</div>
 	</div>
-</div>
 
-<?php endif; ?>
+	<?php endif; ?>
+
+	<div id="content">
